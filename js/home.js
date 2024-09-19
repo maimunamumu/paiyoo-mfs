67e01ec7048a580c25490426e7811842927623f1
Maimuna <maimunamumu7@gmail.com>
2024-09-19 08:48:17 +0600

    third commit done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
-console.log('clicked here')
+// add money to the account
+document.getElementById('btn-add-money')
+   .addEventListener('click',function(event){
+    event.preventDefault();
+//    step-2
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    // step-3 get the pin provide
+    const pinNumberInput=document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput,pinNumberInput);
+});
